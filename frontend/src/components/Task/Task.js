2597f1a54fd93a2b1f624cd2797dcd991a0b9df1
Nuno Machado <nunom4chado@gmail.com>
2021-06-13 17:54:47 +0100

    adds tooltip to completed tasks

--- frontend/src/components/Task/Task.js
+++ frontend/src/components/Task/Task.js
@@ -1,23 +1,33 @@
+import ReactTooltip from "react-tooltip";
 import "./Task.css";
 
 function Task({ task, handleCompleteTask, handleDeleteTask }) {
   return (
-    <div className="form-check c-task">
-      <input
-        className="form-check-input"
-        type="checkbox"
-        disabled={task.completedAt}
-        checked={task.completedAt}
-        onChange={() => handleCompleteTask(task.id)}
-      />
-      <label className="form-check-label">{task.description}</label>
-      {!task.completedAt && (
-        <i
-          className="btn-link bi bi-trash"
-          onClick={() => handleDeleteTask(task.id)}
-        ></i>
-      )}
-    </div>
+    <>
+      <div
+        className="form-check c-task"
+        data-tip={
+          task.completedAt &&
+          `✅ Completed at: ${new Date(task.completedAt).toString()}`
+        }
+      >
+        <input
+          className="form-check-input"
+          type="checkbox"
+          disabled={task.completedAt}
+          checked={task.completedAt}
+          onChange={() => handleCompleteTask(task.id)}
+        />
+        <label className="form-check-label">{task.description}</label>
+        {!task.completedAt && (
+          <i
+            className="btn-link bi bi-trash"
+            onClick={() => handleDeleteTask(task.id)}
+          ></i>
+        )}
+      </div>
+      <ReactTooltip type="dark" />
+    </>
   );
 }
 
